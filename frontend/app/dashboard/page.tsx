841c977d90c3b6f7892c4edb17c0eff4d3f79c8d
Aman Singh <coolaman6299@gmail.com>
2026-07-10 10:14:32 +0530

    Rendered the wallet balance in the frontend

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,8 +1,25 @@
+"use client";
 
-export default function page() {
+import api from "@/lib/api";
+import { useEffect, useState } from "react"
+
+export default function Dashboard() {
+
+  const [ balance, setBalance ] = useState();
+
+  useEffect(() => {
+    const fetchWallet = async () => {
+      const res = await api.get("/api/auth/getWallet");
+      setBalance(res.data.wallet.balance)
+    };
+    fetchWallet();
+  },[]);
+
   return (
     <div>
       <h1>Dashboard</h1>
+      <h2>Wallet Balance</h2>
+      <p>{balance}</p>
     </div>
   )
 }
